4a2e12ddda00749a6e07e3cbb8520fc57d9297f4
ozanustundag <76705999+ozanustundag@users.noreply.github.com>
2021-12-24 13:10:28 +0300

    24 december

---
+++ arrays.playground/Contents.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+var myFavoriteMovies = ["Pulp Fiction","Avengers","Spiderman",5,true] as [Any]
+
+//as-> casting
+// any-> any object
+
+//index
+myFavoriteMovies[0]
+myFavoriteMovies[1]
+myFavoriteMovies[2]
+myFavoriteMovies[3]
+myFavoriteMovies[4]
+
+var myStringArray = ["on","12","on sekiz"]
+myStringArray[0].uppercased()
+myStringArray.count
+myStringArray.last
+myStringArray[myStringArray.count-1]
+myStringArray.sort()
+
+var myNumberArray = [1,2,3]
--- variables.playground/Contents.swift
+++ variables.playground/Contents.swift
@@ -1,3 +1,48 @@
 import UIKit
+// variables and constants
+//snake_case
+//camelCase
+var str = "hello,ozan"
+var myNumber = 5*4
+1*50
+var userName = "ozan"
+var userSurname = "karadağ"
+print(userName)
+userName="lars"
+print(userName)
+//let userAge=5
 
-var greeting = "Hello, playground"
+//let pi = 3.14
+
+//userAge*pi
+//string
+userName.append("k")
+userName.uppercased()
+userName.lowercased()
+
+//integer & double & float
+let userAge = 5
+let myNumberI = 4
+userAge/myNumber
+//Double
+let userAgeD = 5.0
+let myNumberD=4.0
+userAgeD/myNumberD
+//boolean
+
+var myboolean = true
+ 
+//part 2---------
+
+let myString : String = "SAMUEL BECKETT"
+let anotherNumber: Int=10
+
+let stringNumber: String = String(20)
+
+//define
+let myVariable : String
+//initilization
+myVariable = "Deneme"
+let myDownVariable = myVariable.lowercased()
+print(myVariable)
+print(myDownVariable)
