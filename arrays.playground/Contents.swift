8cf1ac5c27389317e2560ff0deb82587b60e6a51
ozanustundag <76705999+ozanustundag@users.noreply.github.com>
2021-12-25 10:29:24 +0300

    25 dECEMBER -1

--- arrays.playground/Contents.swift
+++ arrays.playground/Contents.swift
@@ -1,5 +1,5 @@
 import UIKit
-
+//arrays
 var myFavoriteMovies = ["Pulp Fiction","Avengers","Spiderman",5,true] as [Any]
 
 //as-> casting
@@ -20,3 +20,24 @@ myStringArray[myStringArray.count-1]
 myStringArray.sort()
 
 var myNumberArray = [1,2,3]
+myNumberArray.append(5)
+myNumberArray.last
+//set
+// unordered collection, unique elements
+var mySet : Set = [1,2,3,4,5]
+var myStringSet : Set = ["a","b","c"]
+
+var myInternetArray = [1,2,5,47,8,58,5,5,6,74,7,7]
+var myInternetSet = Set(myInternetArray)
+print(myInternetArray)
+print(myInternetSet)
+
+var mySet1 : Set = [1,2,3]
+var mySet2 : Set = [3,4,5]
+
+var mySet3 = mySet1.union(mySet2)
+print(mySet3)
+
+
+
+
