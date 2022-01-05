f48917c933ae8dd97ce9d3560cc8bc016a95a005
ozanustundag <76705999+ozanustundag@users.noreply.github.com>
2022-01-05 13:25:54 +0300

    5 january - sourcetee test

---
+++ GitTestProject/GitTestProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTestProject
+//
+//  Created by Ozan USTUNDAG on 5.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+print("fdff")
+        
+    }
+
+
+}
+
